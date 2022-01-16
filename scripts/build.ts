a2f68db10ac276db29e043eb25d91033f788413f
Craig Miller <craigmiller160@gmail.com>
2022-01-16 15:46:40 -0500

    Added build script

---
+++ scripts/build.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import spawn from 'cross-spawn';
+import {SpawnSyncReturns} from 'child_process';
+
+const runCommand = (command: string): SpawnSyncReturns<Buffer> => {
+    console.log(`Command: ${command}`);
+    const result = spawn.sync('bash', ['-c', command], {
+        stdio: 'inherit'
+    });
+
+    if (result.status ?? 1 > 0) {
+        throw new Error('Command failed')
+    }
+
+    return result;
+}
+
+runCommand('rm -rf build');
+runCommand('tsc');
+runCommand('cp -R src/cert build')
