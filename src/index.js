f3a13585579d77173d8e8fe803a3e38cc786a8e8
Craig Miller <craigmiller160@gmail.com>
2022-01-16 14:27:24 -0500

    Integrating mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { connect } = require('@craigmiller160/covid-19-config-mongo');
 
 const app = express();
 app.get('/hello', (req, res) => {
     console.log('Hello request received');
     res.send('Hello World');
+});
+
+app.get('/data', async (req, res) => {
+    const count = await connect(async (db) => {
+        return await db.collection('data').count();
+    });
+    res.send(`Count: ${count}`);
 });
 
 app.listen(8080, () => {
