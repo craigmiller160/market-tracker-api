900f5ae16c122f1d3984f0829bf2d5dfc23b0ec8
Craig <craigmiller160@gmail.com>
2022-01-18 20:36:24 -0500

    Progress

--- src/services/auth/RefreshExpiredToken.ts
+++ src/services/auth/RefreshExpiredToken.ts
@@ -1,3 +1,9 @@
+import * as JWT from 'jsonwebtoken';
+import { AccessToken } from '../../express/TokenValidation';
+
+// TODO I want to re-use the same token validation logic that passport uses, rather than have two different operations
+
 export const refreshExpiredToken = (token: string | null) => {
-	throw new Error();
+	const result = JWT.decode(token!) as AccessToken;
+	console.log('Decoded', result.userId);
 };
