391250928c49e62365fee758d3de005ddeaffe86
Craig Miller <craigmiller160@gmail.com>
2022-01-19 06:37:05 -0500

    Began implementing some more refresh logic

--- src/services/auth/RefreshExpiredToken.ts
+++ src/services/auth/RefreshExpiredToken.ts
@@ -1,9 +1,20 @@
 import * as JWT from 'jsonwebtoken';
 import { AccessToken } from '../../express/TokenValidation';
+import { pipe } from 'fp-ts/function';
+import * as Option from 'fp-ts/Option';
+import * as Either from 'fp-ts/Either';
+import * as Try from '@craigmiller160/ts-functions/Try';
+import { UnauthorizedError } from '../../error/UnauthorizedError';
 
 // TODO I want to re-use the same token validation logic that passport uses, rather than have two different operations
 
+const decodeToken = (token: string): Try.Try<AccessToken> =>
+	Try.tryCatch(() => JWT.decode(token) as AccessToken);
+
 export const refreshExpiredToken = (token: string | null) => {
-	const result = JWT.decode(token!) as AccessToken;
-	console.log('Decoded', result.userId);
+	pipe(
+		Option.fromNullable(token),
+		Either.fromOption(() => new UnauthorizedError('No token to refresh')),
+		Either.chain(decodeToken)
+	);
 };
