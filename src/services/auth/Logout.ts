7027ec696a6872e96471b1740e338bf213c60c2e
Craig <craigmiller160@gmail.com>
2022-01-23 16:59:25 -0500

    Code cleanup

--- src/services/auth/Logout.ts
+++ src/services/auth/Logout.ts
@@ -2,29 +2,28 @@ import { AppRefreshTokenModel } from '../../mongo/models/AppRefreshTokenModel';
 import { Request } from 'express';
 import { AccessToken } from '../../express/auth/AccessToken';
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
+import * as Option from 'fp-ts/Option';
+import * as TaskEither from 'fp-ts/TaskEither';
 import * as TaskTry from '@craigmiller160/ts-functions/TaskTry';
 import { UnauthorizedError } from '../../error/UnauthorizedError';
 import { getEmptyCookie } from './Cookie';
+import { TaskTryT } from '@craigmiller160/ts-functions/types';
 
-const deleteRefreshToken = (
-	token: AccessToken
-): TE.TaskEither<Error, unknown> =>
+const deleteRefreshToken = (token: AccessToken): TaskTryT<unknown> =>
 	TaskTry.tryCatch(() =>
 		// TODO do not do this
 		AppRefreshTokenModel.deleteOne({ tokenId: token.jti }).exec()
 	);
 
-export const logout = (req: Request): TE.TaskEither<Error, string> =>
+export const logout = (req: Request): TaskTryT<string> =>
 	pipe(
-		O.fromNullable(req.user as AccessToken | undefined),
-		TE.fromOption(
+		Option.fromNullable(req.user as AccessToken | undefined),
+		TaskEither.fromOption(
 			() =>
 				new UnauthorizedError(
 					'Should not be able to call /logout without being authenticated'
 				)
 		),
-		TE.chainFirst(deleteRefreshToken),
-		TE.chain(() => TE.fromEither(getEmptyCookie()))
+		TaskEither.chainFirst(deleteRefreshToken),
+		TaskEither.chain(() => TaskEither.fromEither(getEmptyCookie()))
 	);
