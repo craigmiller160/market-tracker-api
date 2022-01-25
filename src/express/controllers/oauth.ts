c496bfec752d0385153fde0a2d888ee08c9cd2a3
Craig Miller <craigmiller160@gmail.com>
2022-01-25 15:55:21 -0500

    Trying to figure out how to re-organize functions

--- src/express/controllers/oauth.ts
+++ src/express/controllers/oauth.ts
@@ -1,9 +1,11 @@
 import { secure } from '../auth/secure';
 import * as oAuthService from '../../services/routes/OAuthService';
-import { ReaderT } from '@craigmiller160/ts-functions/types';
+import { ReaderT, TaskT } from '@craigmiller160/ts-functions/types';
 import { ExpressDependencies } from '../ExpressDependencies';
 import { Route } from '../Route';
 import * as Reader from 'fp-ts/Reader';
+import * as Task from 'fp-ts/Task';
+import { Request, Response, NextFunction } from 'express';
 
 export const getAuthUser: ReaderT<ExpressDependencies, Route> = secure(
 	oAuthService.getAuthUser
@@ -16,3 +18,21 @@ export const authCodeAuthentication: ReaderT<ExpressDependencies, Route> =
 		(deps) => (req, res, next) =>
 			oAuthService.authCodeAuthentication(req, res, next)(deps)()
 	);
+
+// TODO clean up below here
+
+const doSomething = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): TaskT<string> => {
+	return Task.of('');
+};
+
+const temp =
+	(deps: ExpressDependencies): Route =>
+	(req, res, next) =>
+		doSomething(req, res, next)();
+
+export const authCodeAuthentication2: ReaderT<ExpressDependencies, Route> =
+	Reader.asks(temp);
