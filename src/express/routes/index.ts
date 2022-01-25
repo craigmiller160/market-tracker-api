6c55fb6243f6e38cf02edda00c2dde5e2fa6c7e4
Craig Miller <craigmiller160@gmail.com>
2022-01-25 14:55:54 -0500

    code cleanup

--- src/express/routes/index.ts
+++ src/express/routes/index.ts
@@ -6,12 +6,10 @@ import * as Reader from 'fp-ts/Reader';
 import { ExpressDependencies } from '../ExpressDependencies';
 import { ReaderT } from '@craigmiller160/ts-functions/types';
 
-export const createRoutes: ReaderT<ExpressDependencies, void> = (
-	dependencies
-): unknown =>
+export const createRoutes: ReaderT<ExpressDependencies, void> =
 	Reader.sequenceArray([
 		createPortfolioRoutes,
 		createWatchlistRoutes,
 		healthcheck,
 		createOAuthRoutes
-	])(dependencies);
+	]);
