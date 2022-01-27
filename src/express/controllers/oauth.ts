921a5a1b6c48067c8152aad410a8ef0f37c61dbb
Craig <craigmiller160@gmail.com>
2022-01-27 17:47:48 -0500

    Trying to figure out a solution for authCodeAuthentication

--- src/express/controllers/oauth.ts
+++ src/express/controllers/oauth.ts
@@ -2,12 +2,20 @@ import { secure, secureReaderTask } from '../auth/secure';
 import * as oAuthService from '../../services/routes/OAuthService';
 import * as Reader from 'fp-ts/Reader';
 import { Controller } from './Controller';
+import { NextFunction, Response, Request } from 'express';
+import { ReaderTaskT } from '@craigmiller160/ts-functions/types'
+import { ExpressDependencies } from '../ExpressDependencies';
+import { ReaderTaskRoute } from '../Route';
 
 export const getAuthUser: Controller = secure(oAuthService.getAuthUser);
 
 export const getAuthCodeLogin: Controller = Reader.of(
 	oAuthService.getAuthCodeLogin
 );
+
+// TODO take care of this... somehow
+const foo: ReaderTaskRoute<string> = (req: Request, res: Response, next: NextFunction): ReaderTaskT<ExpressDependencies, string> =>
+	oAuthService.authCodeAuthentication(req, res, next);
 
 export const authCodeAuthentication: Controller = Reader.asks(
 	(deps) => (req, res, next) =>
---
+++ src/express/controllers/portfolios.ts
@@ -0,0 +1,2 @@
+export {};
+// export const getPortfoliosForUser = (req, res, next)
