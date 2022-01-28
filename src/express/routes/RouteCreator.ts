d1ee7525cb76f08342193573279f0bf732fbe5a9
Craig <craigmiller160@gmail.com>
2022-01-28 07:54:40 -0500

    Code cleanup

--- src/express/routes/RouteCreator.ts
+++ src/express/routes/RouteCreator.ts
@@ -1,6 +1,4 @@
 import { ExpressDependencies } from '../ExpressDependencies';
 import { ReaderT } from '@craigmiller160/ts-functions/types';
 
-export type OldRouteCreator = ReaderT<ExpressDependencies, void>;
-
 export type RouteCreator = ReaderT<ExpressDependencies, void>;
