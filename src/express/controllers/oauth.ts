97687c5780f03c31f27382761f6fce4f5c8af975
Craig <craigmiller160@gmail.com>
2022-01-28 07:38:27 -0500

    Finally fixed that route that looked ugly

--- src/express/controllers/oauth.ts
+++ src/express/controllers/oauth.ts
@@ -2,10 +2,7 @@ import { secure, secureReaderTask } from '../auth/secure';
 import * as oAuthService from '../../services/routes/OAuthService';
 import * as Reader from 'fp-ts/Reader';
 import { Controller } from './Controller';
-import { NextFunction, Response, Request } from 'express';
-import { ReaderTaskT } from '@craigmiller160/ts-functions/types'
-import { ExpressDependencies } from '../ExpressDependencies';
-import { ReaderTaskRoute } from '../Route';
+import { readerTaskRouteToReaderRoute } from '../readerTaskRouteToReaderRoute';
 
 export const getAuthUser: Controller = secure(oAuthService.getAuthUser);
 
@@ -13,13 +10,8 @@ export const getAuthCodeLogin: Controller = Reader.of(
 	oAuthService.getAuthCodeLogin
 );
 
-// TODO take care of this... somehow
-const foo: ReaderTaskRoute<string> = (req: Request, res: Response, next: NextFunction): ReaderTaskT<ExpressDependencies, string> =>
-	oAuthService.authCodeAuthentication(req, res, next);
-
-export const authCodeAuthentication: Controller = Reader.asks(
-	(deps) => (req, res, next) =>
-		oAuthService.authCodeAuthentication(req, res, next)(deps)()
+export const authCodeAuthentication: Controller = readerTaskRouteToReaderRoute(
+	oAuthService.authCodeAuthentication
 );
 
 export const logout: Controller = secureReaderTask(
