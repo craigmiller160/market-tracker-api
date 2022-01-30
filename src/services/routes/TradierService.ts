a3c9b9d8faec1ace2c609be434c81f80d41c84cf
Craig Miller <craigmiller160@gmail.com>
2022-01-30 09:18:48 -0500

    Began stubbing out tradier route

---
+++ src/services/routes/TradierService.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from 'express';
+import { restClient } from '../RestClient';
+
+export const queryTradier = (req: Request, res: Response, next: NextFunction) => {
+    // Finish this
+}
