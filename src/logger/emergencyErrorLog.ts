205521734b75e4f0e42163cdb12505b5d4239816
Craig Miller <craigmiller160@gmail.com>
2022-02-07 15:07:59 -0500

    Working to add emergency error log support

---
+++ src/logger/emergencyErrorLog.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import * as Option from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import * as File from '@craigmiller160/ts-functions/File';
+import * as Either from 'fp-ts/Either';
+
+const LOG_FILE_PATH = path.join(process.cwd(), 'emergency-log.txt');
+type BooleanString = 'true' | 'false';
+
+const useEmergencyErrorLog = (): BooleanString =>
+	pipe(
+		Option.fromNullable(
+			process.env.USE_EMERGENCY_ERROR_LOG as BooleanString | undefined
+		),
+		Option.getOrElse((): BooleanString => 'false')
+	);
+
+export const emergencyErrorLog = (timestamp: string, err: Error) => {
+	if (useEmergencyErrorLog() === 'true') {
+        fs.writeFileSync(LOG_FILE_PATH, timestamp, {
+
+        })
+		pipe(
+			File.writeFileSync(LOG_FILE_PATH, `${timestamp}\n`),
+			Either.chain(() =>
+				File.writeFileSync(LOG_FILE_PATH, `${err.stack ?? ''}\n`)
+			)
+		);
+	}
+};
