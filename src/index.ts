630640b86b3e6e76f0c4fb3d9e3f0cfa08f7ecf0
Craig Miller <craigmiller160@gmail.com>
2022-02-09 16:05:42 -0500

    Restored commented code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import './processErrorHandling'; TODO restore this
+import './processErrorHandling';
 import { connectToMongo } from './mongo';
 import { pipe } from 'fp-ts/function';
 import * as TaskEither from 'fp-ts/TaskEither';
