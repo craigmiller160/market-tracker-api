ed8e0c01a739002aff3245673141be11b0d1c7be
Craig Miller <craigmiller160@gmail.com>
2022-02-09 08:46:29 -0500

    Experimenting with webpack build

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.join(process.cwd(), 'src', 'index.ts'),
+	mode: process.env.NODE_ENV,
+	target: 'node',
+	output: {
+		path: path.join(process.cwd(), 'build'),
+		filename: 'index.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	}
+};
