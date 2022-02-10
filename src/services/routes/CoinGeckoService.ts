5a41c2a48054b806aa609f233759806208585c66
Craig Miller <craigmiller160@gmail.com>
2022-02-10 13:02:05 -0500

    Added coingecko base url and stubbed out query function

---
+++ src/services/routes/CoinGeckoService.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+import { TaskT } from '@craigmiller160/ts-functions/types';
+
+export const queryCryptoGecko = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): TaskT<void> => {};
