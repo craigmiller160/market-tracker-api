33bc9d7d02d75edcb8829c06065c0989755c9dbd
Craig Miller <craigmiller160@gmail.com>
2022-02-13 10:34:14 -0500

    Played with how request paths work

--- src/services/routes/TempService.ts
+++ src/services/routes/TempService.ts
@@ -2,5 +2,5 @@
 import { Request, Response } from 'express';
 
 export const hello = (req: Request, res: Response) => {
-	res.send(`Hello ${req.path}`);
+	res.send(`Hello ${req.baseUrl} ${req.path}`);
 };
