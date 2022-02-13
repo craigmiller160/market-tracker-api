c2792c47bafa72e643f16863feaf8d962da9108b
Craig Miller <craigmiller160@gmail.com>
2022-02-13 15:01:43 -0500

    More progress on new architecture for individual routes

--- src/express/routes/portfolios.ts
+++ src/express/routes/portfolios.ts
@@ -2,16 +2,17 @@ import { Router } from 'express';
 import * as Reader from 'fp-ts/Reader';
 import { pipe } from 'fp-ts/function';
 import * as portfolioController from '../controllers/portfolios';
-import { Route } from '../Route';
+import { Route, TaskRoute } from '../Route';
 import { newSecureRouter } from './routeUtils';
 import { ReaderT } from '@craigmiller160/ts-functions/types';
 import { ExpressRouteDependencies } from '../ExpressDependencies';
+import * as portfolioService from '../../services/routes/PortfolioService';
 
 // TODO majorly refactor and cleanup
 
 interface RouterAndRoutes {
 	readonly router: Router;
-	readonly getPortfoliosForUser: Route;
+	readonly getPortfoliosForUser: TaskRoute<void>;
 	// readonly savePortfoliosForUser: Route;
 }
 
@@ -21,7 +22,7 @@ const configureRoutes = ({
 	getPortfoliosForUser
 }: // savePortfoliosForUser
 RouterAndRoutes): Router => {
-	router.get('/', getPortfoliosForUser);
+	router.get('/', (req, res, next) => getPortfoliosForUser(req, res, next)());
 	// router.post('/', savePortfoliosForUser);
 	return router;
 };
@@ -32,7 +33,7 @@ export const createPortfolioRoutes: ReaderT<ExpressRouteDependencies, void> =
 		Reader.bindTo('router'),
 		Reader.bind(
 			'getPortfoliosForUser',
-			() => portfolioController.getPortfoliosForUser
+			() => portfolioService.getPortfoliosByUser2
 		),
 		// Reader.bind(
 		// 	'savePortfoliosForUser',
--- src/services/routes/PortfolioService.ts
+++ src/services/routes/PortfolioService.ts
@@ -3,9 +3,43 @@ import { pipe } from 'fp-ts/function';
 import { AccessToken } from '../../express/auth/AccessToken';
 import * as TaskEither from 'fp-ts/TaskEither';
 import { Portfolio } from '../../data/modelTypes/Portfolio';
-import { ExpressDependencies } from '../../express/ExpressDependencies';
-import { ReaderTaskT } from '@craigmiller160/ts-functions/types';
+import {
+	ExpressDependencies,
+	ExpressRouteDependencies
+} from '../../express/ExpressDependencies';
+import {
+	ReaderT,
+	ReaderTaskT,
+	TaskT
+} from '@craigmiller160/ts-functions/types';
 import { errorTask } from '../../function/Route';
+import { Route, TaskRoute } from '../../express/Route';
+import * as ReaderTaskEither from 'fp-ts/ReaderTaskEither';
+import * as Reader from 'fp-ts/Reader';
+import * as ReaderTask from 'fp-ts/ReaderTask';
+import { PortfolioRepository } from '../../data/repo/PortfolioRepository';
+
+// TODO rename this
+export const getPortfoliosByUser2: ReaderT<
+	ExpressRouteDependencies,
+	TaskRoute<void>
+> = pipe(
+	Reader.asks<ExpressRouteDependencies, PortfolioRepository>(
+		({ portfolioRepository }) => portfolioRepository
+	),
+	Reader.map(
+		(portfolioRepository) =>
+			(req: Request, res: Response, next: NextFunction) => {
+				const token = req.user as AccessToken;
+				return pipe(
+					portfolioRepository.findPortfoliosForUser(token.userId),
+					TaskEither.fold(errorTask(next), (_) => async () => {
+						res.json(_);
+					})
+				);
+			}
+	)
+);
 
 export const getPortfoliosByUser =
 	(
