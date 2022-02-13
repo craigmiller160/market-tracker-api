e41f00be7665ed0c7bd59f84d51197e12c0ae39c
Craig Miller <craigmiller160@gmail.com>
2022-02-13 15:41:14 -0500

    Deleted old controller code and fixed broken test logic

--- src/express/controllers/Controller.ts
+++
@@ -1,5 +0,0 @@
-import { ExpressDependencies } from '../ExpressDependencies';
-import { Route } from '../Route';
-import { ReaderT } from '@craigmiller160/ts-functions/types';
-
-export type Controller = ReaderT<ExpressDependencies, Route>;
--- src/express/controllers/tradier.ts
+++
@@ -1,5 +0,0 @@
-import * as tradierService from '../../services/routes/TradierService';
-import { Controller } from './Controller';
-import { secureTask } from '../auth/secure';
-
-export const queryTradier: Controller = secureTask(tradierService.queryTradier);
