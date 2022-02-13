998a558b9c5268cbb4d7d3307676e153bbd9963e
Craig Miller <craigmiller160@gmail.com>
2022-02-13 11:36:26 -0500

    Took notes on the actual solution

--- src/express/auth/secure2.ts
+++ src/express/auth/secure2.ts
@@ -1,17 +1,21 @@
-import { Route } from '../Route';
 import passport from 'passport';
 import { Request, Response, NextFunction } from 'express';
+
+// TODO use this as another middleware, as opposed to wrapping the request
+// TODO perform the refresh and set the response cookie
+// TODO call next() to propagate to the actual handler
 
 export const secure2 = (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) =>
+): void =>
 	passport.authenticate(
 		'jwt',
 		{ session: false },
 		(error, user, tokenError) => {
-			console.log('URL', req.originalUrl);
-			next(tokenError);
+			// req.originalUrl
+			// req.method
+			next();
 		}
 	)(req, res, next);
