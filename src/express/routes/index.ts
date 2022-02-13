a66eb319e103ae019510ab6dfd766af669681c7f
Craig Miller <craigmiller160@gmail.com>
2022-02-13 14:38:41 -0500

    Integrated the creation of secure with dependencies into express setup

--- src/express/routes/index.ts
+++ src/express/routes/index.ts
@@ -12,38 +12,7 @@ import { createTradierRoutes } from './tradier';
 import { createCoinGeckoRoutes } from './coingecko';
 import { createSecure2 } from '../auth/secure2';
 
-export const createRoutes: ReaderT<ExpressDependencies, void> =
-	Reader.sequenceArray([
-		createPortfolioRoutes,
-		createOAuthRoutes,
-		createHealthcheckRoutes,
-		createWatchlistRoutes,
-		createTradierRoutes,
-		createCoinGeckoRoutes
-	]);
-
-export const createRoutes2: ReaderT<ExpressDependencies, void> = (deps) => {
-	const secure2 = createSecure2({
-		hasRefreshed: false,
-		appRefreshTokenRepository: deps.appRefreshTokenRepository
-	});
-
-	const routeDependencies: ExpressRouteDependencies = {
-		...deps,
-		secure2
-	};
-
-	return Reader.sequenceArray([
-		createPortfolioRoutes,
-		createOAuthRoutes,
-		createHealthcheckRoutes,
-		createWatchlistRoutes,
-		createTradierRoutes,
-		createCoinGeckoRoutes
-	])(routeDependencies);
-};
-
-export const createRoutes3: ReaderT<ExpressDependencies, void> = Reader.asks(
+export const createRoutes: ReaderT<ExpressDependencies, void> = Reader.asks(
 	(deps) => {
 		const secure2 = createSecure2({
 			hasRefreshed: false,
