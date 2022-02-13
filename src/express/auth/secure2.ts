915f78ca1e79502904216a1c3e83c88676c2304a
Craig Miller <craigmiller160@gmail.com>
2022-02-13 13:47:32 -0500

    Setup the dependencies to pass down through the chain

--- src/express/auth/secure2.ts
+++ src/express/auth/secure2.ts
@@ -2,14 +2,23 @@ import passport from 'passport';
 import { Request, Response, NextFunction } from 'express';
 import { AccessToken } from './AccessToken';
 import * as Option from 'fp-ts/Option';
-import { OptionT } from '@craigmiller160/ts-functions/types';
+import { OptionT, ReaderTaskT } from '@craigmiller160/ts-functions/types';
 import { pipe } from 'fp-ts/function';
 import { match } from 'ts-pattern';
-import { isJwtInCookie } from './jwt';
+import { AppRefreshTokenRepository } from '../../data/repo/AppRefreshTokenRepository';
+import * as mongoRefreshTokenRepo from '../../data/repo/mongo/MongoAppRefreshTokenRepository';
 
 // TODO use this as another middleware, as opposed to wrapping the request
 // TODO perform the refresh and set the response cookie
 // TODO call next() to propagate to the actual handler
+
+interface SecureDependencies {
+	readonly req: Request;
+	readonly res: Response;
+	readonly next: NextFunction;
+	readonly refreshTokenRepo: AppRefreshTokenRepository;
+	readonly hasRefreshed: boolean;
+}
 
 const getError = (
 	error: Error | null,
@@ -27,7 +36,10 @@ const tryToRefreshExpiredToken = () => {
 };
 
 // TODO need to pass hasRefreshed in, plus probably req/res/next
-const handleTokenError = (error: Error, next: NextFunction) => {
+const handleTokenError = (
+	error: Error,
+	next: NextFunction
+): ReaderTaskT<SecureDependencies, void> => {
 	match({ error, jwtIsInCookie: false, refreshAlreadyHappened: false })
 		.with(
 			{
@@ -40,6 +52,8 @@ const handleTokenError = (error: Error, next: NextFunction) => {
 		.otherwise(() => next(error));
 };
 
+// TODO how to I get the ExpressDependencies here? Specifically the refresh token repo?
+// TODO I'm going to have to connect this to the broader Reader chain to get the repo
 export const secure2 = (
 	req: Request,
 	res: Response,
@@ -60,7 +74,17 @@ export const secure2 = (
 						req.user = user as AccessToken;
 						next();
 					},
-					(realError) => handleTokenError(realError, next)
+					(realError) =>
+						handleTokenError(
+							realError,
+							next
+						)({
+							req,
+							res,
+							next,
+							hasRefreshed: false,
+							refreshTokenRepo: mongoRefreshTokenRepo
+						})()
 				)
 			);
 		}
