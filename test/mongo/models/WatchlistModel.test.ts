885173d7354d870efbdf3bd77b1e8c8bfee73117
Craig Miller <craigmiller160@gmail.com>
2022-04-06 21:14:48 -0400

    Working on test

--- test/mongo/models/WatchlistModel.test.ts
+++ test/mongo/models/WatchlistModel.test.ts
@@ -1,7 +1,46 @@
-export {};
+import { Watchlist } from '../../../src/data/modelTypes/Watchlist';
+import {
+	createFullTestServer,
+	FullTestServer,
+	stopFullTestServer
+} from '../../testutils/fullTestServer';
+import { WatchlistModel } from '../../../src/mongo/models/WatchlistModel';
 
 describe('WatchlistModel', () => {
+	let fullTestServer: FullTestServer;
+	beforeAll(async () => {
+		fullTestServer = await createFullTestServer();
+	});
+
+	afterAll(async () => {
+		await stopFullTestServer(fullTestServer);
+	});
+
+	afterEach(async () => {
+		await WatchlistModel.deleteMany().exec();
+	});
+
 	it('enforces optimistic locking', async () => {
-		throw new Error();
+		const watchlist: Watchlist = {
+			watchlistName: 'Hello',
+			stocks: [],
+			cryptos: [],
+			userId: 1
+		};
+		await new WatchlistModel(watchlist).save();
+
+		const existing1 = (await WatchlistModel.find().exec())[0];
+		const existing2 = (await WatchlistModel.find().exec())[0];
+
+		existing1.stocks.push({ symbol: 'ABC' });
+		await existing1.save();
+
+		const result = (await WatchlistModel.find().exec())[0];
+		expect(result.toObject()).toEqual({
+			...watchlist,
+			_id: existing1._id,
+			__v: 1,
+			stocks: [{ _id: expect.anything(), symbol: 'ABC' }]
+		});
 	});
 });
