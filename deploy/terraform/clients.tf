a0562e5c41f91c52a376dcafa765021f7990cbe9
Craig Miller <craigmiller160@gmail.com>
2023-02-15 17:06:06 -0500

    Renamed clients

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -8,8 +8,8 @@ data "keycloak_realm" "apps_prod" {
 
 locals {
   client_common = {
-    client_id = "expense-tracker-api"
-    name = "expense-tracker-api"
+    client_id = "market-tracker-api"
+    name = "market-tracker-api"
     enabled = true
     access_type = "CONFIDENTIAL"
     service_accounts_enabled = true
@@ -20,7 +20,7 @@ locals {
   }
 }
 
-resource "keycloak_openid_client" "expense_tracker_api_dev" {
+resource "keycloak_openid_client" "market_tracker_api_dev" {
   realm_id = data.keycloak_realm.apps_dev.id
   client_id = local.client_common.client_id
   name = local.client_common.name
@@ -29,7 +29,7 @@ resource "keycloak_openid_client" "expense_tracker_api_dev" {
   service_accounts_enabled = local.client_common.service_accounts_enabled
 }
 
-resource "keycloak_openid_client" "expense_tracker_api_prod" {
+resource "keycloak_openid_client" "market_tracker_api_prod" {
   realm_id = data.keycloak_realm.apps_prod.id
   client_id = local.client_common.client_id
   name = local.client_common.name
@@ -38,14 +38,14 @@ resource "keycloak_openid_client" "expense_tracker_api_prod" {
   service_accounts_enabled = local.client_common.service_accounts_enabled
 }
 
-resource "keycloak_role" "expense_tracker_api_access_role_dev" {
+resource "keycloak_role" "market_tracker_api_access_role_dev" {
   realm_id = data.keycloak_realm.apps_dev.id
-  client_id = keycloak_openid_client.expense_tracker_api_dev.id
+  client_id = keycloak_openid_client.market_tracker_api_dev.id
   name = local.access_role_common.name
 }
 
-resource "keycloak_role" "expense_tracker_api_access_role_prod" {
+resource "keycloak_role" "market_tracker_api_access_role_prod" {
   realm_id = data.keycloak_realm.apps_prod.id
-  client_id = keycloak_openid_client.expense_tracker_api_prod.id
+  client_id = keycloak_openid_client.market_tracker_api_prod.id
   name = local.access_role_common.name
 }
