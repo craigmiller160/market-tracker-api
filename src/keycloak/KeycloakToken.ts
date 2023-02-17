48fad21ea4e225b383ec300465d7207788302327
Craig Miller <craigmiller160@gmail.com>
2023-02-17 15:50:58 -0500

    Implemented keycloak token and fixing various errors

--- src/keycloak/KeycloakToken.ts
+++ src/keycloak/KeycloakToken.ts
@@ -1,3 +1,6 @@
+import * as Option from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+
 export type KeycloakToken = {
 	readonly exp: number;
 	readonly iat: number;
@@ -19,3 +22,15 @@ export type KeycloakToken = {
 };
 
 export const getUserId = (token: KeycloakToken): string => token.sub;
+
+export const getAllRoles = (token: KeycloakToken): ReadonlyArray<string> => {
+	const resourceRoles = pipe(
+		Option.fromNullable(process.env.CLIENT_ID),
+		Option.chain((clientId) =>
+			Option.fromNullable(token.resource_access[clientId]?.roles)
+		),
+		Option.getOrElse((): ReadonlyArray<string> => [])
+	);
+	const realmRoles = token.realm_access.roles.map((role) => `realm:${role}`);
+	return [...resourceRoles, ...realmRoles];
+};
