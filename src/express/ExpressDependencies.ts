d631dc6aa39cbdad749c1b2f8007658bb125b043
Craig Miller <craigmiller160@gmail.com>
2023-02-17 16:09:43 -0500

    More cleanup

--- src/express/ExpressDependencies.ts
+++ src/express/ExpressDependencies.ts
@@ -2,16 +2,10 @@ import { PortfolioRepository } from '../data/repo/PortfolioRepository';
 import { Express } from 'express';
 import { WatchlistRepository } from '../data/repo/WatchlistRepository';
 import { AppRefreshTokenRepository } from '../data/repo/AppRefreshTokenRepository';
-import { Route } from './Route';
 
 export interface ExpressDependencies {
 	readonly portfolioRepository: PortfolioRepository;
 	readonly watchlistRepository: WatchlistRepository;
 	readonly appRefreshTokenRepository: AppRefreshTokenRepository;
 	readonly expressApp: Express;
-}
-
-// TODO delete this
-export interface ExpressRouteDependencies extends ExpressDependencies {
-
 }
