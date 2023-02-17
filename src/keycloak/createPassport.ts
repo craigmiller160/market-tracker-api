8a0ff7bda39335e1e6e714db18fd9d21ed46fab2
Craig Miller <craigmiller160@gmail.com>
2023-02-16 20:32:14 -0500

    Putting more pieces together for passport

--- src/keycloak/createPassport.ts
+++ src/keycloak/createPassport.ts
@@ -2,6 +2,7 @@ import { TokenValidationConfig } from './getTokenValidationConfig';
 import passport from 'passport';
 import {
 	JwtFromRequestFunction,
+	SecretOrKeyProvider,
 	Strategy as JwtStrategy,
 	StrategyOptions
 } from 'passport-jwt';
@@ -10,12 +11,19 @@ import { logger } from '../logger';
 const jwtFromRequest: JwtFromRequestFunction = (req) =>
 	req.header('Authorization')?.replace('Bearer ', '') ?? null;
 
+const createSecretOrKeyProvider =
+	(keys: Record<string, string>): SecretOrKeyProvider =>
+	(req, jwt, done) => {};
+
 export const createPassport = (
 	tokenValidationConfig: TokenValidationConfig
 ) => {
 	logger.debug('Creating passport JWT validation strategy')();
 	const options: StrategyOptions = {
 		jwtFromRequest,
+		secretOrKeyProvider: createSecretOrKeyProvider(
+			tokenValidationConfig.keys
+		),
 		issuer: tokenValidationConfig.issuer
 	};
 
